35c7d29b4ce7b49b289a4a5768c949e23d223602
Anatoliy L <lubinecaa1991@yandex.ru>
2022-11-27 18:37:04 +0300

    1е ревью

--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,16 @@
-import './data.js'
+import photoCards from './data.js'
+
+
+const elementsTemplate = document.querySelector('#elements-template').content.querySelector('.elements__item');
+const elementsList = document.querySelector('#elements-list');
+
 const imagePopupCloseButton = document.querySelector('.image-popup__close-button');
-const elementsImage = document.querySelector('.elements__photo')
-const deleteElement = document.querySelector('.elements__delete');
-const like = document.querySelector('.elements__heart');
+
+
+// const elementsImage = document.querySelectorAll('.elements__photo');
+// const deleteElement = document.querySelectorAll('.elements__delete');
+// const like = document.querySelectorAll('.elements__heart');
+
 const imagePopup = document.querySelector('.image-popup');
 const addPopup = document.querySelector('.add-popup');
 const popup = document.querySelector('.popup');
@@ -15,6 +23,7 @@ let aboutMeInput = popup.querySelector('.popup__input_description_about-me');
 
 const addFormOfSubmit = addPopup.querySelector('.add-popup__submit-form');
 const addCloseButton = addPopup.querySelector('.add-popup__close-button');
+
 let addNameInput = addPopup.querySelector('.add-popup__input_description_name');
 let addLink = addPopup.querySelector('.add-popup__input_description_link');
 
@@ -25,16 +34,20 @@ let aboutMe = profile.querySelector('.profile__about-me');
 
 
 //Добавление карточки
-const elementsTemplate = document.querySelector('#elements-template').content.querySelector('.elements__item');
+
 
 //Открытие формы
 
-const openImagePopup = function() {
+const openImagePopup = function(evt) {
+  imagePopup.querySelector('.image-popup__img').src = evt.target.src;
+  imagePopup.querySelector('.image-popup__title').textContent = evt.target.parentElement.querySelector('.elements__name').textContent;
   imagePopup.classList.add('image-popup_opened');
 }
 
 const openAddPopup = function() {
 addPopup.classList.add('add-popup_opened');
+addNameInput.value = '';
+addLink.value = '';
 }
 
 const openPopup = function() {
@@ -51,15 +64,18 @@ const closeImagePopup = function() {
 
 const closeAddPopup = function() {
   addPopup.classList.remove('add-popup_opened');
+
 }
 
 const closePopup = function() {
   popup.classList.remove('popup_opened');
+
 }
 
 //Отправка формы
 const addSubmitForm = function(evt) {
   evt.preventDefault();
+  elementsList.prepend(createElement({name: addNameInput.value, link: addLink.value}));
   closeAddPopup();
 }
 
@@ -82,9 +98,10 @@ const deleteCard = function(evt) {
   evt.target.closest('.elements__item').remove();
 }
 
+
+
 //Слушатели событий
 imagePopupCloseButton.addEventListener('click', closeImagePopup);
-elementsImage.addEventListener('click', openImagePopup);
 
 editButton.addEventListener('click', openPopup);
 closeButton.addEventListener('click', closePopup);
@@ -94,6 +111,33 @@ addFormOfSubmit.addEventListener('submit', addSubmitForm);
 addButton.addEventListener('click', openAddPopup);
 addCloseButton.addEventListener('click', closeAddPopup);
 
-like.addEventListener('click', likeHeart);
-deleteElement.addEventListener('click', deleteCard);
+// like.forEach(item =>
+//   item.addEventListener('click', likeHeart));
 
+// deleteElement.forEach(item =>
+//   item.addEventListener('click', deleteCard));
+
+// elementsImage.forEach(item =>
+//   item.addEventListener('click', openImagePopup));
+
+photoCards.forEach(item => {
+  elementsList.prepend(createElement(item))
+});
+
+
+function createElement(item) {
+      const elm = elementsTemplate.cloneNode(true);
+
+      elm.querySelector('.elements__name').textContent = item.name;
+      elm.querySelector('.elements__photo').src = item.link;
+      const elmDeleteButton = elm.querySelector('.elements__delete');
+      const elmLikeButton = elm.querySelector('.elements__heart');
+      const elmImagePopup = elm.querySelector('.elements__photo');
+      elmDeleteButton.addEventListener('click', deleteCard);
+      elmImagePopup.addEventListener('click', openImagePopup);
+      elmLikeButton.addEventListener('click', likeHeart);
+
+      return elm;
+
+
+  }
